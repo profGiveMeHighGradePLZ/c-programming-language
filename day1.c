ec7ef02dd3167333f3e6b45d5beb503306a6eba5
profGiveMeHighGradePLZ <23257024@life.hkbu.edu.hk>
2024-06-16 22:11:24 +0800

    Add files via upload
    day1

---
+++ day1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int c = 0;
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+    scanf("%d %d %d", &a ,&b ,&c);
+    sum = a + b + c;
+    printf("sum = %d\n",sum);
+    return 0;
+}
